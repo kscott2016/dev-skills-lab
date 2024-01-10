bb32448ce10025e96f2ee5a5bfd1dbacfe42be4f
kscott2016 <krystinascott2008@yahoo.com>
2024-01-09 19:34:09 -0600

    add skills data

---
+++ data/skill-data.js
@@ -0,0 +1,6 @@
+const skills=[
+  {name: 'HTML',level:'intermediate', years:5},
+  {name: 'Javascript', level:'novice', years:2},
+  {name:'WordPress', level:'intermediate', years:4},
+  {name: 'CSS', level:'intermediate', years:5},
+]
